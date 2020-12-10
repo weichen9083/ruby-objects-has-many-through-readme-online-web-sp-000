fa11e46299dbf9155d28521752534894edf361f9
wei chen <weichen9083@gmail.com>
2020-12-10 01:52:56 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,19 +1,18 @@
 class Meal
-  
-  attr_accessor :waiter, :customer, :total, :tip 
-  
+ 
+  attr_accessor :waiter, :customer, :total, :tip
+ 
   @@all = []
-  
+ 
   def initialize(waiter, customer, total, tip=0)
     @waiter = waiter
     @customer = customer
     @total = total
     @tip = tip
-    @@all << self 
-  end 
-  
+    @@all << self
+  end
+ 
   def self.all
-    @@all 
-  end 
-
+    @@all
+  end
 end
